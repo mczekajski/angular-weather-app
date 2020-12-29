099307b7bf4aa0703dd7b83193eee00d574c0b74
Marcin Czekajski <czekajskimarcin@gmail.com>
2020-12-29 18:22:33 +0100

    Add getWeatherData function to WeatherService

--- src/app/weather.service.ts
+++ src/app/weather.service.ts
@@ -1,9 +1,29 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+interface WeatherResponse {
+  weather: [{
+    id: number,
+    main: string,
+    description: string,
+    icon: string
+  }],
+  main: {
+    temp: number,
+    feels_like: number,
+    pressure: number,
+    humidity: number
+  }
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class WeatherService {
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
+  public getWeatherData(city: string, key: string) {
+    return this.http.get<WeatherResponse>(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}&lang=pl`)
+  }
+
 }
