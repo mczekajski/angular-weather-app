d7101b581b666acc790b5a1875133b364a5d721b
Marcin Czekajski <czekajskimarcin@gmail.com>
2020-12-29 18:21:05 +0100

    Add WeatherCard component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { WeatherCardComponent } from './weather-card/weather-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WeatherCardComponent
   ],
   imports: [
     BrowserModule
