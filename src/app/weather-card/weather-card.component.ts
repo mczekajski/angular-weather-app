f7aeb94d70df96f059a6f31048e0ec05c09332a2
Marcin Czekajski <czekajskimarcin@gmail.com>
2020-12-30 10:52:23 +0100

    Use back-end server for getting data

--- src/app/weather-card/weather-card.component.ts
+++ src/app/weather-card/weather-card.component.ts
@@ -7,7 +7,6 @@ import { WeatherService } from '../weather.service';
   styleUrls: ['./weather-card.component.scss'],
 })
 export class WeatherCardComponent implements OnInit {
-  key = '';
   city = '';
   iconUrl = '';
   weatherDescription = '';
@@ -22,13 +21,14 @@ export class WeatherCardComponent implements OnInit {
 
   getWeather(event: any) {
     event.preventDefault();
-    this.weather.getWeatherData(this.city, this.key).subscribe((data) => {
-      this.iconUrl = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
-      this.weatherDescription = data.weather[0].description;
-      this.temperature = data.main.temp;
-      this.feels_like = data.main.feels_like;
-      this.humidity = data.main.humidity;
-      this.pressure = data.main.pressure;
+    this.weather.getWeatherData(this.city).subscribe((data) => {
+      console.log(data);
+      this.iconUrl = `http://openweathermap.org/img/wn/${data.results.weather[0].icon}@2x.png`;
+      this.weatherDescription = data.results.weather[0].description;
+      this.temperature = data.results.main.temp;
+      this.feels_like = data.results.main.feels_like;
+      this.humidity = data.results.main.humidity;
+      this.pressure = data.results.main.pressure;
     });
   }
 }
--- src/app/weather.service.ts
+++ src/app/weather.service.ts
@@ -2,28 +2,33 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 interface WeatherResponse {
-  weather: [{
-    id: number,
-    main: string,
-    description: string,
-    icon: string
-  }],
-  main: {
-    temp: number,
-    feels_like: number,
-    pressure: number,
-    humidity: number
-  }
+  results: {
+    weather: [
+      {
+        id: number;
+        main: string;
+        description: string;
+        icon: string;
+      }
+    ];
+    main: {
+      temp: number;
+      feels_like: number;
+      pressure: number;
+      humidity: number;
+    };
+  };
 }
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class WeatherService {
   constructor(private http: HttpClient) {}
 
-  public getWeatherData(city: string, key: string) {
-    return this.http.get<WeatherResponse>(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}&lang=pl`)
+  public getWeatherData(city: string) {
+    return this.http.get<WeatherResponse>(
+      `https://still-earth-15355.herokuapp.com/api/city?q=${city}`
+    );
   }
-
 }
